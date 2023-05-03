bf21ef8c9fc316df9581ff95c83e98117988982c
thisismostafaxx_ <81190585+TheMostafax@users.noreply.github.com>
2023-05-03 18:29:22 +0300

    Update Contact.js

--- client/src/components/pages/Contact.js
+++ client/src/components/pages/Contact.js
@@ -2,35 +2,24 @@ import React from 'react';
 import '../../App.css';
 
 export default function Contact() {
-    return ( <
-        div className = "contact" >
-        <
-        h1 > Contact Us < /h1>
-
-        <
-        form id = "contact-form"
-        method = "POST" >
-        <
-        label htmlFor = "name" > Full Name < /label> <
-        input name = "name"
-        placeholder = "Enter full name..."
-        type = "text" / >
-        <
-        label htmlFor = "email" > Email < /label> <
-        input name = "email"
-        placeholder = "Enter email..."
-        type = "email" / >
-        <
-        label htmlFor = "message" > Message < /label> <
-        textarea id = "txt"
-        rows = "6"
-        placeholder = "Enter message..."
-        name = "message"
-        required >
-        < /textarea> <
-        button type = "submit"
-        id = "bttt" > Send Message < /button> <
-        /form> <
-        /div>
-    );
+    return (
+        <div className="contact">
+            <h1> Contact Us</h1>
+    
+            <form id="contact-form" method="POST">
+              <label htmlFor="name">Full Name</label>
+              <input name="name" placeholder="Enter full name..." type="text" />
+              <label htmlFor="email">Email</label>
+              <input name="email" placeholder="Enter email..." type="email" />
+              <label htmlFor="message">Message</label>
+              <textarea id="txt"
+                rows="6"
+                placeholder="Enter message..."
+                name="message"
+                required
+              ></textarea>
+              <button type="submit" id="bttt"> Send Message</button>
+            </form>
+          </div>
+      );
 }

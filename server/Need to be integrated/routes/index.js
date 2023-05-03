35264f09890694396b3323f5bb6f6fcd674c2fed
thisismostafaxx_ <81190585+TheMostafax@users.noreply.github.com>
2023-05-03 18:36:02 +0300

    Update index.js

--- server/Need to be integrated/routes/index.js
+++ server/Need to be integrated/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated } = require('../config/checkAuth')
 
+
 //------------ Welcome Route ------------//
 router.get('/', (req, res) => {
     res.render('welcome');

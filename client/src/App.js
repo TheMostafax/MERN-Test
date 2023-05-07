588f365c3337b11289eaad6cfa2c7159532dbae8
thisismostafaxx_ <81190585+TheMostafax@users.noreply.github.com>
2023-05-07 17:09:35 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from './components/Navbar';
 import './App.css';
 import Home from './components/pages/Home';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import About from './components/pages/About';
 import Works from './components/pages/Works';
 import Contact from './components/pages/Contact';
